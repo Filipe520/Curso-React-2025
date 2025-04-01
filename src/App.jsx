aae22be92772c76c7426d03b5aa7e0692284c761
Filipe520 <filipealves520@gmail.com>
2025-04-01 17:34:43 -0300

    React Icons

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import SeuNome from './components/SeuNome';
 import { useState } from 'react';
 import Saudacao from './components/SauDacao';
 
-import {BrowserRouter as Router, Routes ,Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Contato from './pages/Contato';
 import Empresa from './pages/Empresa';
@@ -21,6 +21,14 @@ import Home from './pages/Home';
 import NavBar from './components/layout/navBar';
 import Footer from './components/layout/Footer';
 
+// Diversos Icones do FontWesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faEnvelope, faMuseum} from '@fortawesome/free-solid-svg-icons';
+
+// Diversos Icones do FontWesome mais de marcas
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faAmazonPay } from '@fortawesome/free-brands-svg-icons';
 import './App.css'
 
 function App() {
@@ -134,16 +142,90 @@ function App() {
         <code>npm install react-router-dom</code>
 
         <Router>
-          <NavBar/>
+          <NavBar />
 
           <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/contato' element={<Contato />}/>
-            <Route path='/empresa' element={<Empresa />}/>
+            <Route path='/' element={<Home />} />
+            <Route path='/contato' element={<Contato />} />
+            <Route path='/empresa' element={<Empresa />} />
           </Routes>
 
           <Footer />
         </Router>
+      </section>
+
+      <section className='sectionAulas'>
+        <p>Acesse o site para mais detalhes.</p>
+        <a href="https://react-icons.github.io/react-icons/" target='_blank' rel='external'><strong>React Icons</strong></a>
+        <main>
+          <h3>Desse código do fontWesome temos essas marcar aqui. <br /> que é Icones para o react</h3>
+          <h5>Terminal</h5>
+          <code>npm install --save @fortawesome/free-brands-svg-icons</code>
+          <h5>Na área de importações</h5>
+          <code>import [] from '@fortawesome/free-brands-svg-icons';</code> 
+          <h5>Dentro do HTML</h5>
+          <code>FontAwesomeIcon icon=[faHome]</code>
+          <h5>Alguns exemplos:</h5>
+          <ul>
+            GitHub: faGithub
+
+              <li>Twitter: faTwitter</li>
+
+              <li>Facebook: faFacebook</li>
+
+              <li>Instagram: faInstagram</li>
+
+              <li>LinkedIn: faLinkedin</li>
+
+              <li>Google: faGoogle</li>
+
+              <li>YouTube: faYoutube</li>
+
+              <li>Spotify: faSpotify</li>
+
+              <li>Reddit: faReddit</li>
+
+              <li>Twitch: faTwitch</li>
+
+              <li>Pinterest: faPinterest</li>
+
+              <li>Snapchat: faSnapchat</li>
+
+              <li>GitLab: faGitlab</li>
+
+              <li>Vimeo: faVimeo</li>
+
+              <li>Apple: faApple</li>
+
+              <li>Amazon: faAmazon</li>
+
+              <li>PayPal: faPaypal</li>
+          </ul>
+          <p>Ser tiver com alguma duvida, <br /> basta ir, na área de importações, é dentro do elemento do import aberta o Ctrl + spaço.</p>
+          <p>import [ <strong>Ctrl + spaço</strong> ] from '@fortawesome/free-brands-svg-icons';</p>
+        </main>
+        <footer>
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faHome} /> Home
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faEnvelope} /> Contato
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faGithub} /> GitHub
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faFacebook} /> Facebook
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faAmazonPay} /> AmazonPay
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faMuseum} /> Museum
+            </li>
+          </ul>
+        </footer>
       </section>
     </div>
 
@@ -151,4 +233,5 @@ function App() {
   )
 }
 // Parte de Exportações
+
 export default App
